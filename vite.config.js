f1b3ecbb649844329328ca7fc202bba606412d9a
user3456tyhjvv <sammymuigai131@gmail.com>
2025-01-12 15:02:50 +0300

    Fix asset paths for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5800,
-  },
-  base: '/TheOneChoice/', // Match your repository name
+  },base: '/TheOneChoice.com/',
   build: {
     outDir: 'dist',
   },
